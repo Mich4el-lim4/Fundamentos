8174e3cc9f06c47cabc3cf671905c390e9125f55
Michael lima <michael.lima.dev@gmail.com>
2023-03-25 18:30:30 -0300

    estruturas de repetição

--- Java/0.5-Construtores, palavra this, sobrecarga, encapsulamento/0.2-Bank/src/application/Program.java
+++ Java/0.5-Construtores, palavra this, sobrecarga, encapsulamento/0.2-Bank/src/application/Program.java
@@ -12,40 +12,53 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		Client cliente;
-
-		System.out.print("Digite o numero da conta: ");
-		int numeroConta = sc.nextInt();
-
-		System.out.print("Digite o nome do titular: ");
-		String titular = sc.next();
-
-		System.out.print("Deseja faser um deposito inicial (y/n): ");
-		char resposta = sc.next().charAt(0);
 		
-		if (resposta == 'y') {
-			System.out.print("Entre com o valor de deposito inicial: ");
-			double valorInicial = sc.nextDouble();
-			cliente = new Client(numeroConta, titular, valorInicial);
-		} else {
-			cliente = new Client(numeroConta, titular);
+		char novoCliente = 's';
+		
+		while(novoCliente == 's') {
+			
+			System.out.print("Digite o numero da conta: ");
+			int numeroConta = sc.nextInt();
+
+			System.out.print("Digite o nome do titular: ");
+			String titular = sc.next();
+
+			System.out.print("Deseja faser um deposito inicial (y/n): ");
+			char resposta = sc.next().charAt(0);
+			
+			if (resposta == 'y') {
+				System.out.print("Entre com o valor de deposito inicial: ");
+				double valorInicial = sc.nextDouble();
+				cliente = new Client(numeroConta, titular, valorInicial);
+			} else {
+				cliente = new Client(numeroConta, titular);
+			}
+			
+			System.out.println();
+			System.out.print("Numero da conta: " + cliente.getNumeroConta());
+			System.out.print("\nTitular: " + cliente.getNomeTitular());
+			System.out.print("\nExtrato: " + cliente.getValue());
+
+			System.out.println("\n");
+			System.out.print("Valor para deposito: ");
+			double valorDeposito = sc.nextDouble();
+			cliente.deposit(valorDeposito);
+
+			System.out.print("Saldo atual: " + cliente.getValue());
+
+			System.out.println("\n");
+			System.out.print("Valor a sacar: ");
+			double saque = sc.nextDouble();
+			cliente.saca(saque);
+			
+			System.out.println();
+			System.out.println();
+			System.out.print("Nova conta? (s/n): ");
+			novoCliente = sc.next().charAt(0);
+			System.out.println();
 		}
 		
-		System.out.println();
-		System.out.print("Numero da conta: " + cliente.getNumeroConta());
-		System.out.print("\nTitular: " + cliente.getNomeTitular());
-		System.out.print("\nExtrato: " + cliente.getValue());
-
-		System.out.println("\n");
-		System.out.print("Valor para deposito: ");
-		double valorDeposito = sc.nextDouble();
-		cliente.deposit(valorDeposito);
-
-		System.out.print("Saldo atual: " + cliente.getValue());
-
-		System.out.println("\n");
-		System.out.print("Valor a sacar: ");
-		double saque = sc.nextDouble();
-		cliente.saca(saque);
+		System.out.println("Processo encerrado!!!");
 
 		sc.close();
 	}
