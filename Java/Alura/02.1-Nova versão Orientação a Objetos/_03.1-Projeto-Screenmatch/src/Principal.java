da3a5540581370529b22cdfd75d12e7ab449b736
Michael lima <michael.lima.dev@gmail.com>
2023-04-05 22:34:52 -0300

    usando oo

---
+++ Java/Alura/02.1-Nova versão Orientação a Objetos/_03.1-Projeto-Screenmatch/src/Principal.java
@@ -0,0 +1,27 @@
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Filme meuFilme = new Filme();
+
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(5);
+        meuFilme.avalia(9);
+        meuFilme.avalia(8);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorEpisodio(10);
+        lost.exibeFichaTecnica();
+    }
+}
