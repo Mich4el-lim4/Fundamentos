aeb1daf2162becc66932b06d64c60fdd76c598b6
Michael lima <michael.lima.dev@gmail.com>
2023-04-21 11:01:58 -0300

    .

---
+++ Java/Cod3r/Seção 3 Fundamentos/Converções/src/fundamentos/Desafio.java
@@ -0,0 +1,30 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Desafio {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Informe o primeiro salário:");
+		String valor1 = sc.next().replace(",", ".");
+		
+		System.out.print("Informe o segundo salário:");
+		String valor2 = sc.next().replace(",", ".");
+		
+		System.out.print("Informe o terceiro salário:");
+		String valor3 = sc.next().replace(",", ".");
+
+		double salario1 = Double.parseDouble(valor1);
+		double salario2 = Double.parseDouble(valor2);
+		double salario3 = Double.parseDouble(valor3);
+		
+		double media = (salario1 + salario2 + salario3) / 3;
+		System.out.println("A média dos salários é " + media);
+		
+		sc.close();
+	}
+
+}
---
+++ Java/Cod3r/Seção 3 Fundamentos/Converções/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author micha
+ *
+ */
+module Converção_String_para_numerico {
+	requires java.desktop;
+}
